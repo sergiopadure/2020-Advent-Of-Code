c63158c386e1839ecccc8343d34c06ac8cebb2b3
Padure Sergio <70955746+sergiopadure@users.noreply.github.com>
2020-12-26 21:35:48 +0100

    Initial work challenge 5

--- 05/01.ps1
+++ 05/01.ps1
@@ -9,3 +9,16 @@ $scriptdir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $dateandtime = Get-Date -Format "dd_MM_yyyy_HH-mm"
 $data = Get-Content -Path "$scriptdir\01.txt"
 $count = 0
+
+function Get-SeatID {
+    param (
+        [Parameter(Mandatory)]
+        [string]$BoardingPass
+    )
+    $chars = $BoardingPass.toCharArray()
+    $row = $chars[0..6] -replace 'F', '0' -replace 'B', '1'
+    $column = $chars[7..9] -replace 'R', '1' -replace 'L', '0'
+    
+}
+
+Get-SeatID -BoardingPass "FFFBFBFRRR"
